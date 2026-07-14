2362175448e14263a8b7012d085ac17be2cee6ec
20240516040009JiangFan <2247256679@qq.com>
2026-07-15 02:15:30 +0800

    添加了软件图标

--- qml/main.cpp
+++ qml/main.cpp
@@ -2,10 +2,13 @@
 //         * 移除 main.cpp 对 DatabaseManager 的直接创建和初始化
 //     [v0.1.3] ZhouChengWei    2026-07-02 23:36:00
 //         * 添加了信号忽略，防止传文件时对面中途退出导致本机因为信号闪退
+//     [v0.1.4] ZhouChengWei    2026-07-15 02:13:31
+//         * 添加了文件图标
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <signal.h>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +16,7 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    app.setWindowIcon(QIcon(":/qt/qml/se/qt/messager/source/messager.png"));
     QCoreApplication::setOrganizationName("se.qt.messager");
     QCoreApplication::setApplicationName("se.qt.messager");
 
